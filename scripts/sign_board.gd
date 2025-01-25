943a115a2ae64d5cab1294fe7e33869e8314808e
Parth <frostgameing0@gmail.com>
2025-01-26 01:27:05 +0530

    added signs and lore

--- scripts/sign_board.gd
+++ scripts/sign_board.gd
@@ -1,17 +1,13 @@
 extends Area2D
 
-@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
-@onready var label: Label = $VBoxContainer/Label
+@onready var label: Label = $Label
 
-
-@export var text: String
-
-
-func _ready():
-	label.text = text
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
 	label.visible = false
 
 
+# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	# Check for overlapping bodies
 	var overlapping_bodies = get_overlapping_bodies()
