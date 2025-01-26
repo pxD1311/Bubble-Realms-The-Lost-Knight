fed831ec80f9a2631eea7b4c91e2dd1530516480
Parth <frostgameing0@gmail.com>
2025-01-26 08:16:22 +0530

    levels completed, level selection system secured.

--- scripts/level_selector.gd
+++ scripts/level_selector.gd
@@ -19,14 +19,14 @@ func _on_level_3_pressed():
 
 func _on_level_4_pressed():
 	if GameManager.levels_unlocked >= 4:
-		get_tree().change_scene_to_file("res://scenes/levels/level_2.tscn")
-		GameManager.current_scene_path = "res://scenes/levels/level_2.tscn"
+		get_tree().change_scene_to_file("res://scenes/levels/level_4.tscn")
+		GameManager.current_scene_path = "res://scenes/levels/level_4.tscn"
 
 
 func _on_level_5_pressed():
 	if GameManager.levels_unlocked >= 5:
-		get_tree().change_scene_to_file("res://scenes/levels/level_2.tscn")
-		GameManager.current_scene_path = "res://scenes/levels/level_2.tscn"
+		get_tree().change_scene_to_file("res://scenes/levels/level_5.tscn")
+		GameManager.current_scene_path = "res://scenes/levels/level_5.tscn"
 
 
 func _on_hub_pressed():
